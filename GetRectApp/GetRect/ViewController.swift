40d72a31703f677fbd828d28239c786b2d67f787
evanfrawley <frawley@uw.edu>
2016-05-20 14:34:29 -0700

    added initial view controllers and connect the spotify sdk to the app

--- GetRectApp/GetRect/ViewController.swift
+++ GetRectApp/GetRect/ViewController.swift
@@ -8,14 +8,32 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    let ClientID = "1a475789c4004e6584ad764a80430f52"
+    let CallBackURL = "getRect://returnAfterLogin"
+    let TokenSwapURL = "http://localhost:1234/swap"
+    let ToeknRefreshServiceURL = "http://localhost:1234/refresh"
+    
+    
+    @IBOutlet weak var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
 
+    @IBAction func loginWithSpotify(sender: AnyObject) {
+        //let auth = SPTAuth.defaultInstance()
+        
+        let loginURL = SPTAuth.loginURLForClientId(ClientID, withRedirectURL: NSURL(string: CallBackURL), scopes: [SPTAuthStreamingScope], responseType: "code")
+        
+        UIApplication.sharedApplication().openURL(loginURL)
+        NSLog("woohoo!")
+        
+        
+    }
     
     
     override func didReceiveMemoryWarning() {
