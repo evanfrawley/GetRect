7fbfdf57bdb24f1549b1e60431fa4a8bb3868692
evanfrawley <frawley@uw.edu>
2016-05-24 16:32:36 -0700

    added CocoaPods

---
+++ GetRectApp/Pods/Target Support Files/Pods-GetRect/Pods-GetRect-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GetRect : NSObject
+@end
+@implementation PodsDummy_Pods_GetRect
+@end
---
+++ GetRectApp/Pods/Target Support Files/Pods-GetRectUITests/Pods-GetRectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GetRectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GetRectUITests
+@end
