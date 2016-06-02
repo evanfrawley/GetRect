06a7bf93747c79c9603d646de11e0838bd1c510b
evanfrawley <evanfrawley@gmail.com>
2016-06-01 22:01:44 -0700

    added map view controller and code

---
+++ GetRectApp/GetRect/MapViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MapViewController.swift
+//  GetRect
+//
+//  Created by Evan on 6/1/16.
+//  Copyright © 2016 iGuest. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let initalLocation = CLLocation(latitude: 47.6062, longitude: -122.3321)
+    
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
